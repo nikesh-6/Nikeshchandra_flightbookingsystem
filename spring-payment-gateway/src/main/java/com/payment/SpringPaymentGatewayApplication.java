a43e376a2512ed0c8492d98cda65c86c37910e57
nikesh-6 <92864240+nikesh-6@users.noreply.github.com>
2021-12-30 22:20:08 +0530

    Add files via upload

---
+++ spring-payment-gateway/src/main/java/com/payment/SpringPaymentGatewayApplication.java
@@ -0,0 +1,25 @@
+package com.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class SpringPaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaymentGatewayApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/payment").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+
+}
